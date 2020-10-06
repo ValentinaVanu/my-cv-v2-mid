e83bb956b41d690a106db005d7d0307e36d59ef7
unknown <vanuvalentina16@gmail.com>
2020-10-06 14:48:12 +0100

    Starting to use axios

--- src/component/welcome/welcome.component.jsx
+++ src/component/welcome/welcome.component.jsx
@@ -1,11 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button } from '../button';
 import { Column } from '../column';
 import { Icon } from '../icomoon';
 import { SectionTitle } from '../section-title';
 import * as WS from './welcome.style'
+import { get } from 'axios'
+
 
 const Welcome = () => {
+  const [welcome, updateWelcome] = useState({})
+
+  useEffect(
+    () => {
+      // get("http://localhost:3456/welcome")
+      //   .then(({ data }) => {
+      //     console.log(data)
+      //     updateWelcome(data)
+      //   })
+
+      // async await
+      const getResult = async() => {
+        const { data } = await get("http://localhost:3456/welcome")
+        updateWelcome(data)
+      }
+      getResult()
+    },
+    []
+  )
+
+  const {
+    person,
+    jobTitle,
+    sectionDescription,
+    sectionTitle
+  } = welcome
+  console.log(jobTitle)
+  debugger
   return (
     <>
       <Column>
@@ -13,10 +43,10 @@ const Welcome = () => {
       </Column>
       <Column height={10} />
       <Column>
-        <WS.StyledH1>Valentina Vanu</WS.StyledH1>
+        <WS.StyledH1>{person && person.firstName} {person && person.lastName} </WS.StyledH1>
       </Column>
       <Column>
-        <WS.StyledH2>Junior Web Developer</WS.StyledH2>
+        <WS.StyledH2>{jobTitle}</WS.StyledH2>
       </Column>
       <Column>
         <WS.StyledH3>About Me</WS.StyledH3>
