8a277494c7005188a9979945cfa3a6cc0f94289c
unknown <vanuvalentina16@gmail.com>
2020-10-06 21:34:08 +0100

    Added more data in json and a info component

---
+++ src/component/connect-info/connect-info.component.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const socialWrapper = () => {
+  return (
+    <div></div>
+  )
+}
+export { socialWrapper }
+
+// maybe i ll use it maybe i ll put it back with connect.component & connect.style
---
+++ src/component/connect-info/connect-info.style.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+
+export const StyledSocial = styled.div `
+  box-shadow: 0 2px 6px rgba(0, 0, 0, 0.25)
+  
+`
