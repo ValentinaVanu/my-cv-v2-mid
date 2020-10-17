d7b8d991a9454c8170319ab102a33b6fb5f8971b
unknown <vanuvalentina16@gmail.com>
2020-10-17 16:03:20 +0100

    Added a job list

--- src/component/experience/experience.component.jsx
+++ src/component/experience/experience.component.jsx
@@ -3,6 +3,7 @@ import { get } from 'axios'
 import { Column } from '../column';
 import { SectionTitle } from '../section-title';
 import { Button } from '../button';
+import { EachJob, JobWrapper } from './experience.styled';
 
 const Experience = () => {
   const [experience, updateExp] = useState ({
@@ -23,7 +24,8 @@ const Experience = () => {
     []
   )
   const {
-    sectionTitle
+    sectionTitle,
+    jobList = [],
   } = experience
   return (
     <>
@@ -34,6 +36,22 @@ const Experience = () => {
     <Column height={10} />
     <Column>
       <div>Hello</div>
+    </Column>
+    <Column height={10} />
+    <Column>
+      <JobWrapper >
+        {jobList.map(job => {
+          return (
+            <EachJob>
+              <div>{job.iconPath}</div>
+              <div>{job.jobTitle}</div>
+              <div>{job.startDate} - {job.endDate}</div>
+              <div>{job.companyName}</div>
+              <div>moreInfoList</div>
+            </EachJob>
+              )
+            })}
+      </JobWrapper>
     </Column>
     <Column height={10} />
       <Column display="flex">
---
+++ src/component/experience/experience.styled.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const EachJob = styled.div `
+  border-bottom: 2px solid #808080;
+  padding: 20px;
+`
+
+export const JobWrapper = styled.div `
+  border-radius: 6px;
+  font-size: 14px;
+  line-height: 30px;
+  color: #808080;
+  border: 2px solid #808080;
+`
