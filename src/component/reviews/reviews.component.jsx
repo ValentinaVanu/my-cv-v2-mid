e0211ced2347127289b147aca8c588e71ad65346
unknown <vanuvalentina16@gmail.com>
2020-10-31 18:03:01 +0000

    Finished Reviews Section

--- src/component/reviews/reviews.component.jsx
+++ src/component/reviews/reviews.component.jsx
@@ -3,6 +3,14 @@ import { get } from 'axios'
 import { Column } from '../column'
 import { SectionTitle } from '../section-title'
 import { Button } from '../button'
+import { Avatar, EachContact, RecomContact, RecommText } from '../recommendations/recommendations.styled'
+import { ReviewDate, ReviewName } from './reviews.styled'
+import Chloe from '../../assets/image/recommendations/chloe.JPG'
+import ChloeSecond from '../../assets/image/recommendations/chloe2.jpg'
+import ChloeThird from '../../assets/image/recommendations/chloe3.jpg'
+
+
+const reviewImgList = [Chloe, ChloeSecond, ChloeThird]
 
 const Reviews = () => {
   const[reviews, updateReviews] = useState({})
@@ -22,12 +30,34 @@ const Reviews = () => {
     []
   )
   const {
-    sectionTitle
+    sectionTitle,
+    reviewList = []
   } = reviews
   return (
     <>
       <Column>
         <SectionTitle>{sectionTitle}</SectionTitle>
+      </Column>
+      <Column height={10} />
+      <Column height={10} />
+      <Column>
+        {reviewList.map((review, key )=> {
+          return (
+            <div>
+              <EachContact>
+                <Avatar src={reviewImgList[key]} alt="img" width="50" />
+                <RecomContact>
+                  <ReviewName>{review.name}</ReviewName>
+                  <ReviewDate>{review.date}</ReviewDate>
+                </RecomContact>
+              </EachContact>
+              <RecommText>{review.text}</RecommText>
+              <Column height={10} />
+              <Column height={10} />
+              <Column height={10} />
+            </div>
+          )
+        })}
       </Column>
       <Column height={10} />
       <Column height={10} />
--- src/component/reviews/reviews.styled.js
+++ src/component/reviews/reviews.styled.js
@@ -1,1 +1,16 @@
 import styled from 'styled-components'
+
+
+export const ReviewDate = styled.div `
+  color: #1875F0;
+  font-size: 14px;
+  font-weight: medium;
+  line-height: 20px;
+`
+
+export const ReviewName = styled.div `
+  font-size: 22px;
+  color: #808080;
+  font-weight: medium;
+  line-height: 30px;
+`
