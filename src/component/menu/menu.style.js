2e5ed29529e0848e04e2dc6e8d9cec2db01de7bc
unknown <vanuvalentina16@gmail.com>
2020-11-11 18:29:19 +0000

    Added some more styling to menu

--- src/component/menu/menu.style.js
+++ src/component/menu/menu.style.js
@@ -1,12 +1,35 @@
 import styled from 'styled-components'
 
 
+export const CloseSection = styled.div `
+  display: flex;
+  cursor: pointer;
+  padding: 5px;
+  &:hover {
+    background-color: #FF006D;
+    color: #fff;
+    cursor: pointer;
+  }
 
-export const MenuListWrap = styled.div `
-  padding: 10px;
-  direction: rtl;
 `
 
+export const StyleMenuIcon = styled.div `
+  cursor: pointer;
+  padding-right: 50px;
+`
+
+export const EachSection = styled.div `
+  padding: 4px 14px;
+  &:hover {
+    background-color: #FF006D;
+    color: #fff;
+    cursor: pointer;
+  }
+`
+export const MenuListWrap = styled.div `
+  /* padding: 10px; */
+  direction: rtl;
+`
 export const BurgerMenu = styled.div `
   position: fixed;
   bottom: 20px;
@@ -15,5 +38,5 @@ export const BurgerMenu = styled.div `
   border: 2px solid #707070;
   box-shadow: 0 2px 6px rgba(0,0,0,0.16);
   border-radius: 6px;
-  padding: 12px 14px;
+
 `
