3a7126615e4b452d289ea34f80dee9a748bd12ac
unknown <vanuvalentina16@gmail.com>
2020-11-14 14:50:22 +0000

    One more fix

--- src/component/menu/menu.style.js
+++ src/component/menu/menu.style.js
@@ -36,8 +36,8 @@ export const MenuListWrap = styled.div `
 `
 export const BurgerMenu = styled.div `
   position: fixed;
-  bottom: 80px;
-  right: 40px;
+  bottom: 20px;
+  right: 20px;
   background-color: #fff;
   border: 2px solid #707070;
   box-shadow: 0 2px 6px rgba(0,0,0,0.16);
